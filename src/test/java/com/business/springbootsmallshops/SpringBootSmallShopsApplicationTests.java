7f6fa085135983c098030468577f50dd2511dd92
orhan.metin <orhan.metin@etiya.com>
2022-02-15 15:40:40 +0300

    Create project
    First commit

---
+++ src/test/java/com/business/springbootsmallshops/SpringBootSmallShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.springbootsmallshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSmallShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

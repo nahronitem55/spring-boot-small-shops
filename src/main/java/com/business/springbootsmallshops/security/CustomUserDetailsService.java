9794ec5fc38b4889998d95375a2b8dc1ff62f8fa
orhan.metin <orhan.metin@etiya.com>
2022-04-19 14:54:51 +0300

    security
    add spring security first commit

---
+++ src/main/java/com/business/springbootsmallshops/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.business.springbootsmallshops.security;
+
+import com.business.springbootsmallshops.model.User;
+import com.business.springbootsmallshops.service.IUserService;
+import com.business.springbootsmallshops.util.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAutohority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user).id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
---
+++ src/main/java/com/business/springbootsmallshops/util/SecurityUtils.java
@@ -0,0 +1,13 @@
+package com.business.springbootsmallshops.util;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class SecurityUtils {
+    public static final String ROLE_PREFIX = "ROLE_";
+
+    public static SimpleGrantedAuthority convertToAutohority(String role){
+        String formattedRole = role.startsWith(ROLE_PREFIX) ? role : ROLE_PREFIX + role;
+
+        return new SimpleGrantedAuthority(formattedRole);
+    }
+}
